5dc3d501815137fc2baccd6ed1cdb8d534f5275a
fatih <121682537+fatihsofts@users.noreply.github.com>
2023-09-03 18:39:03 +0300

    404 Page has updated.

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -1,5 +1,22 @@
-import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Button } from "@nextui-org/react";
 
 export default function Page404() {
-  return <div>Page404</div>;
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex flex-col items-center justify-center mt-16 gap-y-8">
+      <h1 className="text-6xl font-bold text-red-600">404</h1>
+      <p className="text-2xl font-bold text-gray-600">
+        Oops! Looks like you are lost.
+      </p>
+      <Button
+        color="primary"
+        className="font-semibold "
+        onClick={() => navigate("/")}
+      >
+        Home
+      </Button>
+    </div>
+  );
 }
